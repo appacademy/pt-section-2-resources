21dd74b7c9e9074d600a31ce58e2411cbfc95ea3
Jacob <yakenorth@gmail.com>
2023-12-06 23:26:08 -0500

    week 19 day 3 middleware discussion

--- 19-week/3-day/middleware-discussion/app.js
+++ 19-week/3-day/middleware-discussion/app.js
@@ -3,12 +3,45 @@ const users = require("./data.js");
 
 const app = express();
 
-app.get("/users/:userId", (req, res) => {
+app.use(express.json());
+
+// 1. Middleware that exists outside routers
+app.use("/stuff", (req, res, next) => {
+    console.log("Hello from outer middleware");
+    next();
+});
+// 2. Middleware that exists inside routers
+const middlewareFunc = (req, res, next) => {
+    console.log("Hello from inner middleware");
+    next();
+};
+
+app.get("/users/:userId", middlewareFunc, (req, res, next) => {
     const { userId } = req.params;
 
     if (users[userId]) {
         return res.json(users[userId]);
     }
+
+    const err = new Error("User not found!");
+    err.statusCode = 404;
+    next(err);
+});
+
+app.use((req, res, next) => {
+    console.log("Hey you won't see this log");
+    next();
+});
+
+// 3. Error handling middleware
+app.use((err, req, res, next) => {
+    // res.statusCode = 404;
+    res.status(404);
+
+    return res.json({
+        status: err.statusCode,
+        message: err.message,
+    });
 });
 
 const port = 8000;
