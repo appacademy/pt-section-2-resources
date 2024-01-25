551dc5872803decc9c9b09990ea7dd60e893dfa5
Zaviar Brown <brown.zaviar@gmail.com>
2024-01-24 18:40:37 -0700

    EST code-along

---
+++ 21-week/3-day/est/2_models/server/db/models/color.js
@@ -0,0 +1,49 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+    class Color extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    Color.init(
+        {
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                unique: true,
+            },
+            hue: DataTypes.STRING,
+            rbg: DataTypes.INTEGER,
+            isPrimary: {
+                type: DataTypes.BOOLEAN,
+                allowNull: false,
+                defaultValue: false,
+                validate: {
+                    checkPrimary(value) {
+                        const primaryColors = ["red", "blue", "yellow"];
+
+                        if (
+                            value &&
+                            !primaryColors.includes(this.name.toLowerCase())
+                        ) {
+                            throw new Error(
+                                "Color must be red, blue, or yellow for primary"
+                            );
+                        }
+                    },
+                },
+            },
+        },
+        {
+            sequelize,
+            modelName: "Color",
+        }
+    );
+    return Color;
+};
