726b15657574852cc0f5c2e3c607e4a9522a0214
will <w.duffy@outlook.com>
2024-03-26 17:59:09 -0500

    added w21d2 notes

---
+++ 20-week/Bonus/db/queries/01-sweet-and-sour.sql
@@ -0,0 +1,2 @@
+SELECT fruit_name, flavor, days_old FROM fruits
+ORDER BY days_old;
---
+++ 20-week/Bonus/db/seeders.sql
@@ -0,0 +1,18 @@
+INSERT INTO
+baskets
+VALUES
+(1, 'Rasberry Bananas', 2000),
+(2, 'Veggie Basket', 1500),
+(3, 'Veggie Basket', 1600);
+
+INSERT INTO
+fruits
+VALUES
+(1, 'rasberry', 'sweet', 'red', 2, 1),
+(2, 'apple', 'sour', 'green', 1, 2),
+(3, 'banana', 'sweet', 'yellow', 3, 2),
+(4, 'banana', 'sour', 'yellow', 2, 1),
+(5, 'rasberry', 'sweet', 'yellow', 1, 1),
+(6, 'tomato', 'tasty', 'red', 2, 2),
+(7, 'tomato', 'tasty', 'red', 2, 3),
+(8, 'strawberry', 'sweet', 'red', 2, 2);
