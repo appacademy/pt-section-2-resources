dd5a572ef24684c233fa6059216606d3e9ab0f09
will <w.duffy@outlook.com>
2024-04-13 12:25:30 -0500

    added pagination prep lecture solution

--- 23-week/5-day/pagination-prep/routes/pets.js
+++ 23-week/5-day/pagination-prep/routes/pets.js
@@ -1,5 +1,25 @@
 const express = require("express");
+const { route } = require("express/lib/router");
 const router = express.Router();
 
+let { Pet } = require("../db/models");
+
+router.get("/", async (req, res) => {
+
+let {page, size} = req.query
+
+console.log("page size", page, size)
+
+page = parseInt(page)
+size = parseInt(page)
+
+let pets = await Pet.findAll({
+    limit: size,
+    offset: (page - 1) * size
+})
+
+res.json({"Pets": pets})
+
+});
 
 module.exports = router;
